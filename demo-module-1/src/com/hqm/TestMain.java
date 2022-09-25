902d0b0d44fbe4c18c03a37699006dbd4ef8854b
qmhx11 <2982472371@qq.com>
2022-09-25 21:24:47 +0800

    test分支修改3 增加 hello test

--- demo-module-1/src/com/hqm/TestMain.java
+++ demo-module-1/src/com/hqm/TestMain.java
@@ -3,5 +3,7 @@ package com.hqm;
 public class TestMain {
     public static void main(String[] args) {
         System.out.println("hello git");
+
+        System.out.println("hello test");
     }
 }
